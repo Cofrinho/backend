c448ee6b8aad899db8d5e2ce72fc76a4495bac16
Marcos Schlick <marcosschlick@gmail.com>
2025-06-02 13:16:49 -0300

    feat: register group routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { userRoutes } from './http/routes/UserRoutes.js';
+import { userRoutes } from './http/routes/userRoutes.js';
+import { groupRoutes } from './http/routes/groupRoutes.js';
+import { groupParticipantRoutes } from './http/routes/groupParticipantRoutes.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/groups', groupRoutes);
+app.use('/participants', groupParticipantRoutes);
+
 app.get('/', async (req, res) => {
   res.status(200).json({ ok: 'true' });
 });
