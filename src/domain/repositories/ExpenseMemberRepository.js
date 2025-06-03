e10945dff076e92cb9feda49903abe380d6f907d
Ruan <oliveiraruan2018@gmail.com>
2025-06-03 07:53:55 -0400

    refactor:formatting json response struture

--- src/domain/repositories/ExpenseMemberRepository.js
+++ src/domain/repositories/ExpenseMemberRepository.js
@@ -1,5 +1,5 @@
 import { ExpenseMember } from  '../models/ExpenseMember.js';
-
+import { User } from '../models/User.js';
 class ExpenseMemberRepository{
   async createAll(data){
     const expenseMember = await ExpenseMember.bulkCreate(data);
@@ -10,7 +10,12 @@ class ExpenseMemberRepository{
     const expenseMembers = await ExpenseMember.findAll({
       where: {
         expense_id: expenseId
-      }
+      },
+      include: [{
+        model: User,
+        attributes: ['name']
+      }],
+      attributes: ['amount', 'percentage_paid', 'status']
     });
     return expenseMembers;
   }
--- src/domain/repositories/ExpenseRepository.js
+++ src/domain/repositories/ExpenseRepository.js
@@ -3,14 +3,20 @@ import { Expense } from '../models/Expense.js';
 class ExpenseRepository {
   async findAllByGroup(groupId){
 
-    const expenses = await Expense.findAll({ where: { group_id: groupId }});
+    const expenses = await Expense.findAll({
+      where: { group_id: groupId },
+      attributes: ['name', 'value']
+    });
     return expenses;
 
   }
 
   async findByIdAndGroup(id, groupId){
 
-  const expense = await Expense.findOne({ where: { id, group_id: groupId }});
+  const expense = await Expense.findOne({
+    where: { id, group_id: groupId },
+    attributes: ['name', 'description', 'status', 'value', 'due_date']
+  });
   return expense;
 
   }
