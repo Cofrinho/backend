bef4098bd53988806093e2d966cc75c203349348
Marcos Schlick <marcosschlick@gmail.com>
2025-06-03 18:32:08 -0300

    feat: add mock data to group seeder

--- src/infra/db/seeders/20250603130634-demo-group.cjs
+++ src/infra/db/seeders/20250603130634-demo-group.cjs
@@ -1,35 +1,34 @@
 'use strict';
-const generateRandomCode = require('../../../shared/utils/generateRandomCode.cjs');
 
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('groups', [
       {
-        access_code: generateRandomCode(4),
+        access_code: 'F5H3',
         name: 'Família Almeida',
         description: 'Contas da família',
         group_owner: 1,
       },
       {
-        access_code: generateRandomCode(4),
+        access_code: '537J',
         name: 'Amigos da Faculdade',
         description: 'Despesas compartilhadas entre amigos',
         group_owner: 2,
       },
       {
-        access_code: generateRandomCode(4),
+        access_code: '13H6',
         name: 'Viagem à Praia',
         description: 'Custos da viagem de dezembro',
         group_owner: 3,
       },
       {
-        access_code: generateRandomCode(4),
+        access_code: 'HGFY',
         name: 'Churrasco Mensal',
         description: 'Grupo do churrasco dos amigos',
         group_owner: 4,
       },
       {
-        access_code: generateRandomCode(4),
+        access_code: '34H6',
         name: 'Projeto de Trabalho',
         description: 'Despesas do projeto da empresa',
         group_owner: 5,
