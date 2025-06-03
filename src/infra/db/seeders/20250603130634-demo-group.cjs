9b8c30651861320202fe22d009c619a91643b4b0
Marcos Schlick <marcosschlick@gmail.com>
2025-06-03 15:42:17 -0300

    feat: add reusable random code generator utility

--- src/infra/db/seeders/20250603130634-demo-group.cjs
+++ src/infra/db/seeders/20250603130634-demo-group.cjs
@@ -1,43 +1,36 @@
 'use strict';
 
+const generateRandomCode = require('../../utils/generateRandomCode.js');
+
 module.exports = {
   async up(queryInterface) {
-    const generateAccessCode = () => {
-      const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-      let result = '';
-      for (let i = 0; i < 6; i++) {
-        result += chars.charAt(Math.floor(Math.random() * chars.length));
-      }
-      return result;
-    };
-
     await queryInterface.bulkInsert('groups', [
       {
-        access_code: generateAccessCode(),
+        access_code: generateRandomCode(6),
         name: 'Família Almeida',
         description: 'Contas da família',
         group_owner: 1,
       },
       {
-        access_code: generateAccessCode(),
+        access_code: generateRandomCode(6),
         name: 'Amigos da Faculdade',
         description: 'Despesas compartilhadas entre amigos',
         group_owner: 2,
       },
       {
-        access_code: generateAccessCode(),
+        access_code: generateRandomCode(6),
         name: 'Viagem à Praia',
         description: 'Custos da viagem de dezembro',
         group_owner: 3,
       },
       {
-        access_code: generateAccessCode(),
+        access_code: generateRandomCode(6),
         name: 'Churrasco Mensal',
         description: 'Grupo do churrasco dos amigos',
         group_owner: 4,
       },
       {
-        access_code: generateAccessCode(),
+        access_code: generateRandomCode(6),
         name: 'Projeto de Trabalho',
         description: 'Despesas do projeto da empresa',
         group_owner: 5,
