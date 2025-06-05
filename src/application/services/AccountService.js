1cad2259466ef75f337c6cc5c99962d83a4e5b83
Ruan <oliveiraruan2018@gmail.com>
2025-06-05 19:22:36 -0400

    feat: last transactions and balance of account

--- src/application/services/AccountService.js
+++ src/application/services/AccountService.js
@@ -1,6 +1,10 @@
+import { User } from '../../domain/models/User.js';
 import AccountRepository from '../../domain/repositories/AccountRepository.js';
+import RechargeFundTransactionRepository from '../../domain/repositories/RechargeFundTransRepository.js';
 import UserRepository from '../../domain/repositories/UserRepository.js';
 import { AppError } from '../../shared/errors/AppError.js';
+import ExpensePaymentRepository from '../../domain/repositories/ExpensePaymentRepository.js';
+
 
 export default class AccountService {
   static async getBalance(user_id) {
@@ -25,5 +29,46 @@ export default class AccountService {
     await AccountRepository.incrementBalance(user_id, amount);
 
     return await AccountRepository.getAccount(user_id);
+  }
+
+  static async getInfo(userId){
+    if(!(await User.findByPk(userId))){
+      throw new AppError('user not found', 404);
+    }
+
+    const account = await AccountRepository.getAccount(userId);
+
+    if(!account){
+      throw new AppError('account not found this user', 404);
+    }
+
+    const rechargeTransactions = await RechargeFundTransactionRepository.findAllByUserId(userId);
+    const expensesPayment = await ExpensePaymentRepository.findAllByUserId(userId);
+
+    console.log(expensesPayment);
+
+    const expensesPaymentsMapped = expensesPayment.map(p => ({
+      id: p.id,
+      name: p.Expense.name,
+      date: p.created_at,
+      group: `Grupo ${p.Expense.Group.id}`,
+      value: p.value,
+    }));
+
+    const rechargeTransactionsMapped = rechargeTransactions.map(t => ({
+      id: t.id,
+      value: t.amount,
+      title: 'Recarga Cofrinho',
+      date: t.created_at
+    }));
+
+    const allTransactions = [...rechargeTransactionsMapped, ...expensesPaymentsMapped];
+
+    const lastTransactions = allTransactions.sort((a, b) => b.date - a.date).slice(0, 3);
+
+    return {
+      balance: account.balance,
+      transactions: lastTransactions
+    }
   }
 }
--- src/http/routes/accountRoutes.js
+++ src/http/routes/accountRoutes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import AccountController from '../controllers/AccountController.js';
 
 const router = new Router();
-
 router.get('/users/:userId/balance', AccountController.getBalance);
+router.get('/home/:userId', AccountController.getInfo);
 
 export { router as accountRoutes };
