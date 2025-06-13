a5c97c0d5e73be25d17aecd7d56c91e7e64f0aa7
Andrei Albrecht <andreialbrecht@gmail.com>
2025-06-13 06:05:16 -0300

    refactor(expense): update ExpenseService logic and structure

--- src/domain/repositories/ExpenseRepository.js
+++ src/domain/repositories/ExpenseRepository.js
@@ -14,6 +14,7 @@ class ExpenseRepository {
     const expense = await Expense.findOne({
       where: { id, group_id: groupId },
       attributes: [
+        'id',
         'name',
         'description',
         'status',
